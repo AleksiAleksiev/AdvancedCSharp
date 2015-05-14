3bb84b722d077fa6497561db696337c536e1f19d
AleksiAleksiev <aleksivaleksiev@gmail.com>
2015-05-14 09:16:43 +0300

    Initial commit

---
+++ MethodsHomework/01.BiggerNumber/BiggerNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class BiggerNumber
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int max = GetMax(firstNumber, secondNumber);
+        Console.WriteLine(max);
+    }
+
+    private static int GetMax(int firstNumber, int secondNumber)
+    {
+        int max = firstNumber;
+        if (secondNumber > firstNumber)
+        {
+            max = secondNumber;
+        }
+        return max;
+    }
+}
+
---
+++ MethodsHomework/04.FirstLargerThanNeigh/FirstLargerThanNeighbours.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class FirstLargerThanNeighbours
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+        int result = -1;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            result = IndexFirstLargerThanNeigbours(numbers, i);
+            if (result != -1)
+            {
+                Console.WriteLine(result);
+                return;
+            }
+        }
+        Console.WriteLine(result);
+    }
+
+    private static int IndexFirstLargerThanNeigbours(List<int> numbers, int i)
+    {
+        if (numbers.Count == 1)
+        {
+            return -1;
+        }
+        if (i == 0)
+        {
+            return (numbers[i] > numbers[i + 1]) ? 0 : -1;
+        }
+        if (i == numbers.Count - 1)
+        {
+            return (numbers[i] > numbers[i - 1]) ? i : -1;
+        }
+        if (numbers[i] > numbers[i + 1] && numbers[i] > numbers[i - 1])
+        {
+            return i;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
+
